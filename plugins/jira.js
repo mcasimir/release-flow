1a1e86be1a97cbbeef91d345cdd382136da984b7
mcasimir <maurizio.cas@gmail.com>
2016-05-16 13:36:35 +0200

    removed unused files

--- plugins/jira.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(release, options) {
-  release.phases.start.before('commit', function npm(release) {
-    let regex = new RegExp(`\\b${options.project}-\\d+\\b`, 'g');
-    release.changelog = release.changelog.replace(regex, `[$1](${options.url}/boards/$1)`);
-  });
-};
