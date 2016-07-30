7d2322b6fda8ae8c43242175f1dedbfcff618a11
mcasimir <maurizio.cas@gmail.com>
2016-07-30 19:12:17 +0200

    wip

--- test/Step.spec.js
+++ test/Step.spec.js
@@ -1,9 +1,16 @@
-import assert from 'assert';
+import {equal} from 'assert';
 import Step from '../src/Step';
+import {spy} from 'sinon';
 
 describe('Step', function() {
-  it.skip('', function() {
-    let step = new Step();
-    assert(step);
+  it('Adds a named step to target class property', function() {
+    class TestClass {
+      @Step('myMethod')
+      method() {
+        spy();
+      }
+    }
+
+    equal(TestClass.steps[0].name, 'myMethod');
   });
 });
---
+++ test/plugins/bump-package-json.spec.js
@@ -0,0 +1,10 @@
+import assert from 'assert';
+import bumpPackageJson from '../../src/plugins/bump-package-json';
+
+describe('plugins', function() {
+  describe('bumpPackageJson', function() {
+    it.skip('', function() {
+      assert(bumpPackageJson);
+    });
+  });
+});
