3d2c4d540879586e5423ab2684d200fe85b7e301
mcasimir <maurizio.cas@gmail.com>
2016-07-31 22:59:39 +0200

    wip: started finish phase implementation

--- src/phases/Finish.js
+++ src/phases/Finish.js
@@ -1,4 +1,41 @@
-import Phase from '../Phase';
+import Phase, {Step} from '../Phase';
 
 export default class Finish extends Phase {
+  @Step()
+  getReleaseInfo(release) {
+    release.branchName = release.git.getCurrentBranch();
+    release.name = release.branchName
+      .slice(0, release.options.releaseBranchPrefix);
+    release.tagName = `${release.options.tagPrefix}${name}`;
+  }
+
+  @Step()
+  validate(release) {
+    let currentBranch = release.git.getCurrentBranch();
+    if (!currentBranch.startsWith(release.options.releaseBranchPrefix)) {
+      throw release.error(
+        'You can only finish a release from a release branch'
+      );
+    }
+  }
+
+  @Step()
+  mergeToProduction(release) {
+    release.git.checkout(release.options.productionBranch);
+    release.git.merge(release.branchName);
+    release.git.pushRef(release.options.productionBranch);
+  }
+
+  @Step()
+  tagProduction(release) {
+    release.git.tag(release.tagName);
+    release.git.pushRef(release.tagName);
+  }
+
+  @Step()
+  mergeBackToDevelopment(release) {
+    release.git.checkout(release.options.developmentBranch);
+    release.git.merge(release.branchName);
+    release.git.pushRef(release.options.developmentBranch);
+  }
 }
--- test/phases/Finish.spec.js
+++ test/phases/Finish.spec.js
@@ -1,9 +1,37 @@
-import assert from 'assert';
+import {throws, doesNotThrow} from 'assert';
+import {stub} from 'sinon';
 import Finish from '../../src/phases/Finish';
+import Release from '../../src/Release';
 
 describe('Finish', function() {
-  it.skip('', function() {
-    let phase = new Finish();
-    assert(phase);
+  beforeEach(function() {
+    this.release = new Release();
+    stub(this.release.logger, 'debug');
+  });
+
+  describe('validate', function() {
+    it('throws if currentBranch is not release branch', function() {
+      this.release.options.releaseBranchPrefix = 'xyz~';
+
+      stub(this.release.git, 'getCurrentBranch').returns('foo');
+
+      let phase = new Finish();
+
+      throws(() => {
+        phase.validate(this.release);
+      }, /You can only finish a release from a release branch$/);
+    });
+
+    it('does not throw if currentBranch is a release branch', function() {
+      this.release.options.releaseBranchPrefix = 'xyz~';
+      stub(this.release.git, 'getCurrentBranch').returns('xyz~1.0.0');
+      stub(this.release.git, 'hasUntrackedChanges').returns(false);
+
+      let phase = new Finish();
+
+      doesNotThrow(() => {
+        phase.validate(this.release);
+      }, /You can only finish a release from a release branch$/);
+    });
   });
 });
