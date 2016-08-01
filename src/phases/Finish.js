36cb00739b530c0d422614ebed0c78a69c771452
mcasimir <maurizio.cas@gmail.com>
2016-08-01 08:27:33 +0200

    wip Finish

--- src/phases/Finish.js
+++ src/phases/Finish.js
@@ -2,26 +2,59 @@ import Phase, {Step} from '../Phase';
 
 export default class Finish extends Phase {
   @Step()
-  getReleaseInfo(release) {
-    release.branchName = release.git.getCurrentBranch();
-    release.name = release.branchName
-      .slice(0, release.options.releaseBranchPrefix);
-    release.tagName = `${release.options.tagPrefix}${name}`;
+  fetch(release) {
+    release.logger.debug('fetching tags and heads');
+    try {
+      release.git.fetchHeadsAndTags();
+    } catch (e) {
+      throw release.error('Unable to fetch tags and heads');
+    }
   }
 
   @Step()
-  validate(release) {
+  getReleaseInfo(release) {
+    release.branchName = release.git.getCurrentBranch();
+    release.name = release.branchName
+      .slice(release.options.releaseBranchPrefix.length);
+    release.tagName = `${release.options.tagPrefix}${release.name}`;
+  }
+
+  @Step()
+  validateReleaseBranch(release) {
     let currentBranch = release.git.getCurrentBranch();
     if (!currentBranch.startsWith(release.options.releaseBranchPrefix)) {
       throw release.error(
         'You can only finish a release from a release branch'
       );
     }
+
+    if (release.git.hasUntrackedChanges()) {
+      throw release.error('You have untracked changes');
+    }
+
+    if (release.git.hasUnpushedCommits()) {
+      throw release.error('You have unpushed changes');
+    }
+  }
+
+  @Step()
+  checkoutProduction(release) {
+    release.git.checkout(release.options.productionBranch);
+  }
+
+  @Step()
+  validateProductionBranch(release) {
+    if (release.git.hasUntrackedChanges()) {
+      throw release.error('You have untracked changes');
+    }
+
+    if (release.git.hasUnpushedCommits()) {
+      throw release.error('You have unpushed changes');
+    }
   }
 
   @Step()
   mergeToProduction(release) {
-    release.git.checkout(release.options.productionBranch);
     release.git.merge(release.branchName);
     release.git.pushRef(release.options.productionBranch);
   }
--- test/phases/Finish.spec.js
+++ test/phases/Finish.spec.js
@@ -1,4 +1,4 @@
-import {throws, doesNotThrow} from 'assert';
+import {equal, throws, doesNotThrow} from 'assert';
 import {stub} from 'sinon';
 import Finish from '../../src/phases/Finish';
 import Release from '../../src/Release';
@@ -9,16 +9,34 @@ describe('Finish', function() {
     stub(this.release.logger, 'debug');
   });
 
-  describe('validate', function() {
+  describe('getReleaseInfo', function() {
+    it('derives info from current branch', function() {
+      this.release.options.releaseBranchPrefix = 'xyz~';
+      this.release.options.tagPrefix = 'vx';
+
+      stub(this.release.git, 'getCurrentBranch').returns('xyz~foo');
+
+      let phase = new Finish();
+      phase.getReleaseInfo(this.release);
+
+      equal(this.release.branchName, 'xyz~foo');
+      equal(this.release.name, 'foo');
+      equal(this.release.tagName, 'vxfoo');
+    });
+  });
+
+  describe('validateReleaseBranch', function() {
     it('throws if currentBranch is not release branch', function() {
       this.release.options.releaseBranchPrefix = 'xyz~';
 
       stub(this.release.git, 'getCurrentBranch').returns('foo');
+      stub(this.release.git, 'hasUntrackedChanges').returns(false);
+      stub(this.release.git, 'hasUnpushedCommits').returns(false);
 
       let phase = new Finish();
 
       throws(() => {
-        phase.validate(this.release);
+        phase.validateReleaseBranch(this.release);
       }, /You can only finish a release from a release branch$/);
     });
 
@@ -26,12 +44,45 @@ describe('Finish', function() {
       this.release.options.releaseBranchPrefix = 'xyz~';
       stub(this.release.git, 'getCurrentBranch').returns('xyz~1.0.0');
       stub(this.release.git, 'hasUntrackedChanges').returns(false);
+      stub(this.release.git, 'hasUnpushedCommits').returns(false);
 
       let phase = new Finish();
 
       doesNotThrow(() => {
-        phase.validate(this.release);
+        phase.validateReleaseBranch(this.release);
       }, /You can only finish a release from a release branch$/);
+    });
+  });
+
+  describe('checkoutProduction', function() {
+
+  });
+
+  describe('validateProductionBranch', function() {
+
+  });
+
+  describe('mergeToProduction', function() {
+    it('checks out the production branch', function() {
+
+    });
+
+    it('merges from derived branchName', function() {
+
+    });
+
+    it('pushes productionBranch', function() {
+
+    });
+  });
+
+  describe('tagProduction', function() {
+    it('tags productionBranch with tagName', function() {
+
+    });
+
+    it('pushes tagName', function() {
+
     });
   });
 });
